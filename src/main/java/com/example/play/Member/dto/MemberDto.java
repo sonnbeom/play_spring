76f2abf9b686e5cb2490227658ab3d4693939951
sonny <dhsmf4245@gmail.com>
2024-03-19 12:02:51 +0900

    UserDto

--- src/main/java/com/example/play/Member/dto/MemberDto.java
+++ src/main/java/com/example/play/Member/dto/MemberDto.java
@@ -1,4 +1,30 @@
 package com.example.play.Member.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
 public class MemberDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String password;
+    @Column(unique = true)
+    private String phone;
+    @Column(name="is_active")
+    private Integer isActive;
+    @Column
+    private String nickname;
+    @Column
+    private LocalDateTime lastLoginAt;
 }
