7c85e895954781137c693dda0c20ab6db016a745
sonny <dhsmf4245@gmail.com>
2024-04-02 13:37:58 +0900

    postLike create

---
+++ src/main/generated/com/example/play/like/post/entity/QPostLike.java
@@ -0,0 +1,64 @@
+package com.example.play.like.post.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QPostLike is a Querydsl query type for PostLike
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPostLike extends EntityPathBase<PostLike> {
+
+    private static final long serialVersionUID = 347091618L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QPostLike postLike = new QPostLike("postLike");
+
+    public final com.example.play.global.common.entity.QBaseEntity _super = new com.example.play.global.common.entity.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Integer> isActive = createNumber("isActive", Integer.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedAt = _super.lastModifiedAt;
+
+    public final com.example.play.member.entity.QMember member;
+
+    public final com.example.play.post.entity.QPost post;
+
+    public QPostLike(String variable) {
+        this(PostLike.class, forVariable(variable), INITS);
+    }
+
+    public QPostLike(Path<? extends PostLike> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QPostLike(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QPostLike(PathMetadata metadata, PathInits inits) {
+        this(PostLike.class, metadata, inits);
+    }
+
+    public QPostLike(Class<? extends PostLike> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new com.example.play.member.entity.QMember(forProperty("member")) : null;
+        this.post = inits.isInitialized("post") ? new com.example.play.post.entity.QPost(forProperty("post"), inits.get("post")) : null;
+    }
+
+}
+
