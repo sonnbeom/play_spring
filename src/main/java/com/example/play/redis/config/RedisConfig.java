25d517de2fd6bf14f547269da4b19b16da9ed0ff
sonny <dhsmf4245@gmail.com>
2024-04-18 09:39:11 +0900

    redis config

---
+++ src/main/java/com/example/play/redis/config/RedisConfig.java
@@ -0,0 +1,4 @@
+package com.example.play.redis.config;
+
+public class RedisConfig {
+}
---
+++ src/main/java/com/example/play/redis/service/RedisService.java
@@ -0,0 +1,9 @@
+package com.example.play.redis.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RedisService {
+}
