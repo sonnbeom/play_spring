9f9aee927c01a288c4f78c97672dce1b1f7ff50a
sonny <dhsmf4245@gmail.com>
2024-04-20 10:57:00 +0900

    WebSocket config

--- src/main/java/com/example/play/global/common/constant/Bucket.java
+++ src/main/java/com/example/play/global/common/constant/Bucket.java
@@ -3,4 +3,8 @@ package com.example.play.global.common.constant;
 abstract public class Bucket {
     public static String MEMBER ="member";
     public static String POST ="post";
+
+    public abstract static class CookieConstants {
+        public static String REFRESH_TOKEN = "refreshToken";
+    }
 }
