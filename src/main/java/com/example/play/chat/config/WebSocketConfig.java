2cdedfc28150f149c81f270cd17b570c72022a01
sonny <dhsmf4245@gmail.com>
2024-04-22 10:22:21 +0900

    WebSocket CustomHandshakeInterceptor

--- src/main/java/com/example/play/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/play/chat/config/WebSocketConfig.java
@@ -1,29 +1,40 @@
 package com.example.play.chat.config;
 
+import com.example.play.chat.interceptor.CustomHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
     private final WebSocketHandler webSocketHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat")
+                .addInterceptors(new HttpSessionHandshakeInterceptor(), new CustomHandshakeInterceptor())
+                        .setAllowedOrigins("https:/localhost/api/v1/chatting");
+
         // endpoint 설정 : /api/v1/chat/{postId}
         // 이를 통해서 ws://localhost:9090/ws/chat 으로 요청이 들어오면 websocket 통신을 진행한다.
         // setAllowedOrigins("*")는 모든 ip에서 접속 가능하도록 해줌
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+//        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
     }
+    // WebSocket 메시지 버퍼의 크기를 설정하는 메소드
+    @Bean
     public ServletServerContainerFactoryBean createWebSocketContainer(){
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxBinaryMessageBufferSize(50000);
-        container.setMaxTextMessageBufferSize(50000);
-        return container;
+        ServletServerContainerFactoryBean containerFactoryBean = new ServletServerContainerFactoryBean();
+        containerFactoryBean.setMaxBinaryMessageBufferSize(100000);
+        containerFactoryBean.setMaxTextMessageBufferSize(100000);
+        return containerFactoryBean;
     }
 }
---
+++ src/main/java/com/example/play/chat/interceptor/CustomHandshakeInterceptor.java
@@ -0,0 +1,37 @@
+package com.example.play.chat.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Log4j2
+public class CustomHandshakeInterceptor implements HandshakeInterceptor {
+    // ws://yourserver.com/websocket?userName=사용자이름" 와 같이 보내면 userName 꺼내서 쓸 수 있다.
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes){
+        // ServerHttpRequest를 ServletServerHttpRequest로 형 변환하여, 원래의 HTTP 요청 객체에 접근합니다.
+        ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+        HttpServletRequest req = servletRequest.getServletRequest();
+        //HTTP 요청으로부터 HttpServletRequest 객체를 얻어 세션 정보(HttpSession)를 추출합니다.
+        HttpSession httpSession = req.getSession();
+        //세션 ID(sessionID)를 attributes 맵에 저장합니다.
+        //이 맵은 웹소켓 세션과 연결되어, 핸드셰이크 동안 및 이후의 처리에서 사용될 수 있습니다.
+        String sessionId = httpSession.getId();
+        attributes.put("sessionId", sessionId);
+        String userName = req.getParameter("userName");
+        attributes.put("userName", userName);
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+        log.info("Handshake complete");
+    }
+}
