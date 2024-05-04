1152f7beabb5878128f0f3e06d2bde63a3ef5312
sonny <dhsmf4245@gmail.com>
2024-05-04 09:41:26 +0900

    test member controller

---
+++ src/test/java/com/example/play/member/controller/MemberControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.play.member.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberControllerTest {
+
+    @Test
+    @DisplayName("컨트롤러: 멤버 생성 테스트")
+    void memberCreateTest(){
+
+    }
+}
