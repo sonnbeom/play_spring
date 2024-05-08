234f90766cc41d36ff61b58479614ff8c2e16780
sonny <dhsmf4245@gmail.com>
2024-05-08 11:12:55 +0900

    add : unit test for post service

---
+++ src/test/java/com/example/play/post/service/PostServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.play.post.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PostServiceTest {
+    @DisplayName("서비스 레이어에서 게시글이 성공적으로 생성되는지 테스트")
+    @Test
+    void postCreateTest(){
+
+    }
+}
