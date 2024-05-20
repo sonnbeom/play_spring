a86eba32633a608a71ffe7692793c9995d70d5fa
sonny <dhsmf4245@gmail.com>
2024-05-20 15:36:43 +0900

    add : test post controller search

--- src/test/java/com/example/play/post/constant/EndPointUrl.java
+++ src/test/java/com/example/play/post/constant/EndPointUrl.java
@@ -3,4 +3,5 @@ package com.example.play.post.constant;
 public class EndPointUrl {
     public static final String TEST_POST_GET_ONE_URL = "/api/v1/post/{postId}";
     public static final String TEST_POST_CREATE_URL = "/api/v1/post/create";
+    public static final String TEST_POST_SEARCH_URL = "/api/v1/post/search";
 }
