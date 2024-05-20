6e6faf58e088dac791d5699680f685ca9b292300
sonny <dhsmf4245@gmail.com>
2024-05-20 16:50:19 +0900

    add : test post controller get liked posts

--- src/test/java/com/example/play/post/constant/EndPointUrl.java
+++ src/test/java/com/example/play/post/constant/EndPointUrl.java
@@ -5,6 +5,7 @@ public class EndPointUrl {
     public static final String TEST_POST_CREATE_URL = "/api/v1/post/create";
     public static final String TEST_POST_SEARCH_URL = "/api/v1/post/search";
     public static final String TEST_POST_SORT_URL = "/api/v1/post/sort";
+    public static final String TEST_POST_GET_LIKED_POST_URL = "/api/v1/post/likeList";
     public static final String TEST_POST_PATCH_URL = "/api/v1/post/{postId}";
     public static final String TEST_POST_DELETE_URL = "/api/v1/post/{postId}";
 
