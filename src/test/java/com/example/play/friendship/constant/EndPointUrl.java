6990c3c2bb1096538c8c453b3acf8fe2e0d11c57
sonny <dhsmf4245@gmail.com>
2024-05-22 13:15:07 +0900

    add : test friendship controller get friendshiplist

--- src/test/java/com/example/play/friendship/constant/EndPointUrl.java
+++ src/test/java/com/example/play/friendship/constant/EndPointUrl.java
@@ -4,4 +4,5 @@ public class EndPointUrl {
     public static String TEST_CREATE_FRIENDSHIP_URL = "/api/v1/friendship/create";
     public static String TEST_GET_WAITING_FRIENDSHIP_URL = "/api/v1/friendship/received";
     public static String TEST_APPROVE_FRIENDSHIP_URL = "/api/v1/friendship/approve/{friendshipId}";
+    public static String TEST_GET_APPROVE_FRIENDSHIP_URL = "/api/v1/friendship/friendList";
 }
