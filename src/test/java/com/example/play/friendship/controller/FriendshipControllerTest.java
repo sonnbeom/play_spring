11b02d7af733024b5d993eb66df55cfb8863197f
sonny <dhsmf4245@gmail.com>
2024-05-22 12:42:09 +0900

    add : test friendship controller get waiting list

--- src/test/java/com/example/play/friendship/controller/FriendshipControllerTest.java
+++ src/test/java/com/example/play/friendship/controller/FriendshipControllerTest.java
@@ -4,11 +4,14 @@ import com.example.play.friendship.constant.EndPointUrl;
 import com.example.play.friendship.constant.FriendshipStatus;
 import com.example.play.friendship.dto.RequestFriendship;
 import com.example.play.friendship.dto.ResponseFriendshipDto;
+import com.example.play.friendship.dto.ResponseFriendshipWithImg;
 import com.example.play.friendship.service.FriendshipService;
+import com.example.play.image.dto.ResponseMemberImg;
 import com.example.play.member.dto.ResponseMemberDto;
 import com.example.play.mock.WithCustomMockUser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,7 +28,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static com.example.play.friendship.constant.EndPointUrl.TEST_FRIENDSHIP_CREATE_URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.play.friendship.constant.EndPointUrl.*;
+import static com.example.play.friendship.constant.FriendshipStatus.WAITING;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -53,11 +60,11 @@ class FriendshipControllerTest {
         //given
         RequestFriendship requestFriendship = new RequestFriendship("receiver@email.com");
         ResponseFriendshipDto result = ResponseFriendshipDto.builder().
-                friendshipId(1L).receiverDto(new ResponseMemberDto()).received(false).senderDto(new ResponseMemberDto()).status(FriendshipStatus.WAITING).build();
+                friendshipId(1L).receiverDto(new ResponseMemberDto()).received(false).senderDto(new ResponseMemberDto()).status(WAITING).build();
         when(friendshipService.create(Mockito.any(RequestFriendship.class), Mockito.anyString())).thenReturn(result);
 
         //when && then
-        mockMvc.perform(MockMvcRequestBuilders.post(TEST_FRIENDSHIP_CREATE_URL)
+        mockMvc.perform(MockMvcRequestBuilders.post(TEST_CREATE_FRIENDSHIP_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(requestFriendship))
                 .with(csrf()))
@@ -65,6 +72,30 @@ class FriendshipControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.received").value(false))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("WAITING"));
+
+    }
+    @Test
+    @DisplayName("친구 컨트롤러: 대기중인 친구요청 리스트 불러오는지 테스트")
+    @WithCustomMockUser
+    void testGetWaitingFriendship() throws Exception {
+        //given
+        List<ResponseFriendshipWithImg> result = new ArrayList<>();
+        ResponseFriendshipDto dto = ResponseFriendshipDto.builder().status(WAITING).build();
+        ResponseMemberImg img = new ResponseMemberImg();
+        ResponseFriendshipWithImg responseFriendshipWithImg = ResponseFriendshipWithImg.builder().friendshipDto(dto).img(img).build();
+        result.add(responseFriendshipWithImg);
+        when(friendshipService.getWaitingFriendList(Mockito.anyString(), Mockito.anyInt())).thenReturn(result);
+
+        //when && then
+        mockMvc.perform(MockMvcRequestBuilders.get(TEST_GET_WAITING_FRIENDSHIP_URL)
+                .param("page", "0")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].friendshipDto.status").value("WAITING"));
+
 
     }
 
