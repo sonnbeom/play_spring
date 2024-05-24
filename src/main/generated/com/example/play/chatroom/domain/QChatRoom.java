b2f9864855628af0af2fe362246dba506d0fa496
sonny <dhsmf4245@gmail.com>
2024-05-24 11:35:21 +0900

    add : test chat controller get

---
+++ src/main/generated/com/example/play/chatroom/domain/QChatRoom.java
@@ -0,0 +1,64 @@
+package com.example.play.chatroom.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QChatRoom is a Querydsl query type for ChatRoom
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QChatRoom extends EntityPathBase<ChatRoom> {
+
+    private static final long serialVersionUID = -1708615701L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QChatRoom chatRoom = new QChatRoom("chatRoom");
+
+    public final com.example.play.global.common.entity.QBaseEntity _super = new com.example.play.global.common.entity.QBaseEntity(this);
+
+    public final ListPath<com.example.play.chat.domain.ChatMessage, com.example.play.chat.domain.QChatMessage> chatMessages = this.<com.example.play.chat.domain.ChatMessage, com.example.play.chat.domain.QChatMessage>createList("chatMessages", com.example.play.chat.domain.ChatMessage.class, com.example.play.chat.domain.QChatMessage.class, PathInits.DIRECT2);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedAt = _super.lastModifiedAt;
+
+    public final com.example.play.member.entity.QMember member;
+
+    public final com.example.play.member.entity.QMember other;
+
+    public QChatRoom(String variable) {
+        this(ChatRoom.class, forVariable(variable), INITS);
+    }
+
+    public QChatRoom(Path<? extends ChatRoom> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QChatRoom(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QChatRoom(PathMetadata metadata, PathInits inits) {
+        this(ChatRoom.class, metadata, inits);
+    }
+
+    public QChatRoom(Class<? extends ChatRoom> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new com.example.play.member.entity.QMember(forProperty("member")) : null;
+        this.other = inits.isInitialized("other") ? new com.example.play.member.entity.QMember(forProperty("other")) : null;
+    }
+
+}
+
