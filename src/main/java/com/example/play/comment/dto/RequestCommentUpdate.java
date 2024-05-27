bad4db0b2e68af219a5d3daf7ec391fc00d14045
sonny <dhsmf4245@gmail.com>
2024-05-27 12:05:24 +0900

    add: Test comment controller update

--- src/main/java/com/example/play/comment/dto/RequestCommentUpdate.java
+++ src/main/java/com/example/play/comment/dto/RequestCommentUpdate.java
@@ -1,8 +1,14 @@
 package com.example.play.comment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestCommentUpdate {
     private Long commentId;
     private String content;
