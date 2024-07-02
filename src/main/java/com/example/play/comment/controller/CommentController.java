5062d5a140bf348e538a374680137ee3ab8ebbfa
son <dhsmf4245@gmail.com>
2024-07-02 11:36:13 +0900

    Feat: 스웨거 config

--- src/main/java/com/example/play/comment/controller/CommentController.java
+++ src/main/java/com/example/play/comment/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.example.play.comment.dto.RequestCommentUpdate;
 import com.example.play.comment.dto.ResponseComment;
 import com.example.play.comment.service.CommentService;
 import com.example.play.jwt.dto.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -28,24 +29,28 @@ public class CommentController {
     * 3. 댓글 수정
     * 4. 댓글 삭제
     * */
+    @Operation(summary = "댓글 생성", description = "게시글에 댓글 생성하는 API입니다.")
     @PostMapping
     public ResponseEntity<ResponseComment>create(@RequestBody RequestCommentCreate commentDto,
                                                  @AuthenticationPrincipal CustomUserDetails customUserDetails){
         ResponseComment result = commentService.create(commentDto, customUserDetails.getUsername());
         return ResponseEntity.status(CREATED).body(result);
     }
+    @Operation(summary = "댓글 조회", description = "게시글에 해당되는 댓글을 조회하는 API입니다.")
     @GetMapping
     public ResponseEntity<List<ResponseComment>>getComments(@RequestParam Long postId,
                                                             @RequestParam(required = false, defaultValue = "0") int page){
         List<ResponseComment> result = commentService.getComments(postId, page);
         return ResponseEntity.status(OK).body(result);
     }
+    @Operation(summary = "댓글 수정", description = "댓글을 수정하는 API입니다.")
     @PatchMapping
     public ResponseEntity<ResponseComment>update(@RequestBody RequestCommentUpdate commentUpdate,
                                                  @AuthenticationPrincipal CustomUserDetails customUserDetails){
         ResponseComment result = commentService.update(commentUpdate, customUserDetails.getUsername());
         return ResponseEntity.status(OK).body(result);
     }
+    @Operation(summary = "댓글 삭제", description = "댓글을 삭제하는 API입니다.")
     @DeleteMapping
     public ResponseEntity<Void>delete(@RequestBody RequestCommentDelete commentDelete,
                                                  @AuthenticationPrincipal CustomUserDetails customUserDetails){
--- src/main/java/com/example/play/member/controller/MemberController.java
+++ src/main/java/com/example/play/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.play.member.controller;
 import com.example.play.jwt.dto.CustomUserDetails;
 import com.example.play.member.dto.*;
 import com.example.play.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,18 +27,21 @@ public class MemberController {
     }
 
     @PostMapping()
+    @Operation(summary = "멤버 회원가입", description = "이메일, 이름, 패스워드, 닉네임, 프로필 사진 등을 받아 회원가입하는 API입니다.")
     public ResponseEntity createMember(@Valid @RequestPart("memberDto") RequestCreateMemberDto memberDto,
                                      @RequestPart(value = "profile", required = false) MultipartFile profile){
         memberService.createMember(memberDto, profile);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "멤버 조회", description = "멤버의 id를 통해 상세 내용을 볼 수 있는 API입니다.")
     @GetMapping("/{memberId}")
     public ResponseEntity<ResponseMemberDto> getMember(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                        @PathVariable Long memberId){
         ResponseMemberDto dto =  memberService.getMember(userDetails.getUsername(), memberId);
         return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
+    @Operation(summary = "멤버 정보 수정", description = "이메일, 패스워드, 닉네임, 프로필 사진 등을 업데이트하는 API입니다.")
     @PatchMapping("/{memberId}")
     public ResponseEntity<ResponseMemberDto> updateMember(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                           @RequestPart(value = "updateDto", required = false) RequestUpdateMemberDto updateDto,
@@ -46,6 +50,7 @@ public class MemberController {
         ResponseMemberDto updatedDto = memberService.updateMember(userDetails.getUsername(), updateDto, profile, deleteFileId);
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
+    @Operation(summary = "멤버 삭제", description = "id를 기반으로 회원삭제하는 API입니다.")
     @DeleteMapping("/{memberId}")
     public ResponseEntity<ResponseDeleteMemberDto> deleteMember(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                                 @PathVariable Long memberId){
